c33e7811baf66fe3b11628053cf47024dca88684
Abraham Meza Vega <abrahammv_6@hotmail.com>
2019-10-05 22:46:01 -0600

    [New] Autoincremental primary keys

--- create.sql
+++ create.sql
@@ -1,89 +1,89 @@
 create database RedDeCuido;
 
 create table Grado(
-	idGrado tinyint primary key not null,
+	idGrado tinyint primary key not null IDENTITY(0, 1),
 	tipo varchar(50) not null default ''
 );
 
 create table Estudio(
-	idEstudio smallint primary key not null,
+	idEstudio smallint primary key not null IDENTITY(0, 1),
 	descripcion varchar(50) not null,
 	idGrado tinyint foreign key references Grado not null,
 	check (descripcion != '')
 );
 
 create table Canton(
-	idCanton tinyint primary key not null,
+	idCanton tinyint primary key not null IDENTITY(0, 1),
 	nombre varchar(30) not null,
 	check (nombre != '')
 );
 
 create table Provincia(
-	idProvincia tinyint primary key not null,
+	idProvincia tinyint primary key not null IDENTITY(0, 1),
 	nombre varchar(30) not null unique,
 	idCanton tinyint foreign key references Canton not null,
 	check (nombre != '')
 );
 
 create table CentroDeAtencion(
-	idCentro smallint primary key not null,
+	idCentro smallint primary key not null IDENTITY(0, 1),
 	nombre varchar(30) not null,
 	idProvincia tinyint foreign key references Provincia not null,
 	check (nombre != '')
 );
 
 create table Comentario(
-	idComentario int primary key not null,
+	idComentario int primary key not null IDENTITY(0, 1),
 	contenido varchar(50) not null default 'El usuario no ha dejado comentario'
 );
 
 create table Calificacion(
-	idCalificacion int primary key not null,
+	idCalificacion int primary key not null IDENTITY(0, 1),
 	puntuacion tinyint not null default 0,
 	idComentario int foreign key references Comentario not null,
 	check (puntuacion >= 0)
 );
 
 create table Horario(
-	idHorario int primary key not null,
+	idHorario int primary key not null IDENTITY(0, 1),
 	tiempo varchar(20) not null,
 	check (tiempo != '')
 );
 
 create table Dia(
-	idDia tinyint primary key not null,
+	idDia tinyint primary key not null IDENTITY(0, 1),
 	nombre varchar(10) not null,
 	check (nombre != '')
 );
 
 create Table Jornada(
-	idJornada int primary key not null,
+	idJornada int primary key not null IDENTITY(0, 1),
 	horaInicio time(0) not null,
 	horaFin time(0) not null,
 	idDia tinyint foreign key references Dia not null
 );
 
 create table HorarioXJornada(
-	id int primary key not null,
+	id int primary key not null IDENTITY(0, 1),
 	idHorario int foreign key references Horario not null,
 	idJornada int foreign key references Jornada not null
 );
 
 create table TipoUsuario(
-	idTipo int primary key not null,
+	idTipo int primary key not null IDENTITY(0, 1),
 	tipo varchar(30) not null,
 	check(tipo != '')
 )
 
 create table Usuario(
-	idUsuario int primary key not null,
+	idUsuario int primary key not null IDENTITY(0, 1),
 	contrasenia varchar(30) not null,
 	idTipo int foreign key references TipoUsuario not null,
 	check(contrasenia != '')
 )
 
 create table Cliente(
-	idCliente int primary key not null,
+	idCliente int primary key not null IDENTITY(0, 1),
 	nombre varchar(30) not null,
 	apellido varchar(30) not null,
 	idProvincia tinyint foreign key references Provincia not null,
@@ -92,7 +92,7 @@ create table Cliente(
 )
 
 create table Personal(
-	idPersonal int primary key not null,
+	idPersonal int primary key not null IDENTITY(0, 1),
 	nombre varchar(30) not null,
 	apellido varchar(30) not null,
 	idCentro smallint foreign key references CentroDeAtencion not null,
@@ -102,43 +102,43 @@ create table Personal(
 )
 
 create table CalificacionXCliente(
-	id int primary key not null,
+	id int primary key not null IDENTITY(0, 1),
 	idCalificacion int foreign key references Calificacion not null,
 	idCliente int foreign key references Cliente not null
 )
 
 create table CalificacionXPersonal(
-	id int primary key not null,
+	id int primary key not null IDENTITY(0, 1),
 	idCalificacion int foreign key references Calificacion not null,
 	idPersonal int foreign key references Personal not null
 )
 
 create table Categoria(
-	idCategoria smallint primary key not null,
+	idCategoria smallint primary key not null IDENTITY(0, 1),
 	descripcion varchar(50) not null,
 	check (descripcion != '')
 )
 
 create table CategoriaXPersonal(
-	id int primary key not null,
+	id int primary key not null IDENTITY(0, 1),
 	idCategoria smallint foreign key references Categoria not null,
 	idPersonal int foreign key references Personal not null
 )
 
 create table EstudioXPersonal(
-	id int primary key not null,
+	id int primary key not null IDENTITY(0, 1),
 	idEstudio smallint foreign key references Estudio not null,
 	idPersonal int foreign key references Personal not null
 )
 
 create table Solicitud(
-	idSolicitud int primary key not null,
+	idSolicitud int primary key not null IDENTITY(0, 1),
 	idPersonal int foreign key references Personal not null,
 	idCliente int foreign key references Cliente not null
 )
 
 create table Contratacion(
-	idContratacion int primary key not null,
+	idContratacion int primary key not null IDENTITY(0, 1),
 	monto money not null default 0,
 	fecha datetime not null default GETDATE(),
 	idSolicitud int foreign key references Solicitud not null,
@@ -146,7 +146,7 @@ create table Contratacion(
 )
 
 create table CategoriaXContratacion(
-	id int primary key not null,
+	id int primary key not null IDENTITY(0, 1),
 	precio money not null default 0,
 	idCategoria smallint foreign key references Categoria not null,
 	idContratacion int foreign key references Contratacion not null,
@@ -154,14 +154,14 @@ create table CategoriaXContratacion(
 )
 
 create table Puesto(
-	idPuesto smallint primary key not null,
+	idPuesto smallint primary key not null IDENTITY(0, 1),
 	nombre varchar(50) not null,
 	idPersonal int foreign key references Personal not null,
 	check(nombre != '')
 )
 
 create table Pago(
-	idPago int primary key not null,
+	idPago int primary key not null IDENTITY(0, 1),
 	fecha datetime not null default GETDATE(),
 	monto money not null default 0,
 	idPersonal int foreign key references Personal not null,
@@ -169,26 +169,26 @@ create table Pago(
 )
 
 create table Actividad(
-	idActividad int primary key not null,
+	idActividad int primary key not null IDENTITY(0, 1),
 	descripcion varchar(50) not null,
 	check(descripcion != '')
 )
 
 create table Correo(
-	idCorreo int primary key not null,
+	idCorreo int primary key not null IDENTITY(0, 1),
 	direccion varchar(50) not null,
 	idUsuario int foreign key references Usuario not null,
 	check(direccion != '')
 )
 
 create table Enfermedad(
-	idEnfermedad smallint primary key not null,
+	idEnfermedad smallint primary key not null IDENTITY(0, 1),
 	nombre varchar(30) not null,
 	check(nombre != '')
 )
 
 create table Tratamiento(
-	idTratamiento int primary key not null,
+	idTratamiento int primary key not null IDENTITY(0, 1),
 	nombre varchar(30) not null,
 	cantidad smallint not null default 0,
 	idEnfermedad smallint foreign key references Enfermedad not null,
@@ -196,25 +196,25 @@ create table Tratamiento(
 )
 
 create table TratamientoXCliente(
-	id int primary key not null,
+	id int primary key not null IDENTITY(0, 1),
 	idTratamiento int foreign key references Tratamiento not null,
 	idCliente int foreign key references Cliente not null
 )
 
 create table TipoServicio(
-	idServicio smallint primary key not null,
+	idServicio smallint primary key not null IDENTITY(0, 1),
 	descripcion varchar(50) not null,
 	check(descripcion != '')
 )
 
 create table ServicioXCentro(
-	id smallint primary key not null,
+	id smallint primary key not null IDENTITY(0, 1),
 	idServicio smallint foreign key references TipoServicio not null,
 	idCentro smallint foreign key references CentroDeAtencion not null
 )
 
 create table EnfermedadXCliente(
-	id int primary key not null,
+	id int primary key not null IDENTITY(0, 1),
 	idEnfermedad smallint foreign key references Enfermedad,
 	idCliente int foreign key references Cliente
 )
